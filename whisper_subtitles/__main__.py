e4260fd1c86aa0a508c0dde4b13120e6f3912f4f
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-03-04 17:02:28 +1300

    Remove audio trim

--- whisper_subtitles/__main__.py
+++ whisper_subtitles/__main__.py
@@ -1,14 +1,3 @@
-"""Script that generates .srt files with subtitles for .mp4 files using OpenAI Whisper library.
-
-Usage:
-- Place the script in the directory containing the .mp4 files to generate subtitles for.
-- Ensure the required dependencies are installed: OpenAI's Whisper library, Python's os module.
-- Run the script using Python interpreter: `python WhisperSubtitles.py`
-
-The script generates .srt files for each .mp4 file found in the directory using Whisper.
-
-Note: Generated subtitles may not be 100% accurate, please correct errors manually.
-"""
 import os
 from utils import write_srt
 import whisper
@@ -22,50 +11,50 @@ def main():
 
     print("Whisper Subtitles script loaded")
 
-    print(f"Loading the {model} language model...")
-
-    model = whisper.load_model(model)
-
-    if model:
-        print("Model loaded successfully")
-    else:
-        return print("Failed to load model. Exiting...")
+    try:
+        print(f"Loading the {model} language model...")
+        model = whisper.load_model(model)
+        if not model:
+            raise Exception("Failed to load model")
+    except Exception as e:
+        print(f"Error: {e}")
+        return
 
     print("Searching for files to subtitle...")
 
-    for file in os.listdir():
-        if file.endswith(tuple(supported_extensions)):
-            # Add to array
-            file_array.append(file)
+    try:
+        for file in os.listdir():
+            if file.endswith(tuple(supported_extensions)):
+                # Add to array
+                file_array.append(file)
+        if len(file_array) == 0:
+            raise Exception("No files found")
+    except Exception as e:
+        print(f"Error: {e}")
+        return
 
     # print number of files added to array
-    if len(file_array) >= 1:
-        print(f"Found {len(file_array)} files to subtitle.")
-    else:
-        return print("No files found. Exiting...")
+    print(f"Found {len(file_array)} files to subtitle.")
 
     for file in file_array:
         print(f"Generating subtitles for {file}...")
 
         # load audio and pad/trim it to fit 30 seconds
         audio = whisper.load_audio(file)
-        audio = whisper.pad_or_trim(audio)
 
         # decode the audio and save as .srt
-        result = whisper.transcribe(
-            model, audio, verbose=True)
-
-        if len(result["segments"]) == 0:
-            print(f"Failed to generate subtitles for {file}. Skipping...")
+        try:
+            result = whisper.transcribe(
+                model, audio, verbose=True)
+            if len(result["segments"]) == 0:
+                raise Exception(f"No subtitles generated for {file}")
+            file_name = os.path.splitext(file)[0]
+            with open(file_name+'.srt', "w", encoding="utf-8") as srt_file:
+                write_srt(result["segments"], file=srt_file)
+            print(f"Subtitles for {file} saved as {file_name}.srt")
+        except Exception as e:
+            print(f"Error: {e}")
             continue
-
-        file_name = os.path.splitext(file)[0]
-
-        with open(file_name+'.srt', "w", encoding="utf-8") as srt_file:
-            write_srt(result["segments"], file=srt_file)
-
-        # success message
-        print(f"Subtitles for {file} saved as {file_name}.srt")
 
 
 if __name__ == '__main__':
