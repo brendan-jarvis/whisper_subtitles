dcc5e932f81a38d9ea6926b54f3b8f2591c1c436
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-03-24 14:16:27 +1300

    Format document

--- whisper_subtitles.py
+++ whisper_subtitles.py
@@ -8,8 +8,7 @@ from utils import write_srt
 def main(args):
     model = args.model
     file_array = []
-    supported_extensions = [".mp4", ".m4a",
-                            ".mp3", ".mpeg", ".mpga", ".wav", ".webm"]
+    supported_extensions = [".mp4", ".m4a", ".mp3", ".mpeg", ".mpga", ".wav", ".webm"]
 
     print("Whisper Subtitles script loaded")
 
@@ -18,8 +17,8 @@ def main(args):
         model = whisper.load_model(model)
         if not model:
             raise Exception("Failed to load model")
-    except Exception as e:
-        print(f"Error: {e}")
+    except Exception as model_error:
+        print(f"Error: {model_error}")
         return
 
     print("Searching for files to subtitle...")
@@ -30,8 +29,8 @@ def main(args):
                 file_array.append(file)
         if len(file_array) == 0:
             raise Exception("No files found")
-    except Exception as e:
-        print(f"Error: {e}")
+    except Exception as file_error:
+        print(f"Error: {file_error}")
         return
 
     print(f"Found {len(file_array)} files to subtitle.")
@@ -39,7 +38,7 @@ def main(args):
     total_time = 0
     for file in file_array:
         file_name = os.path.splitext(file)[0]
-        srt_path = os.path.join(args.output_directory, file_name) + '.srt'
+        srt_path = os.path.join(args.output_directory, file_name) + ".srt"
         if os.path.exists(srt_path):
             print(f"{srt_path} already exists. Skipping...")
             continue
@@ -52,41 +51,74 @@ def main(args):
         try:
             start_time = time.time()
             result = model.transcribe(
-                audio, verbose=True, language=args.language,
-                condition_on_previous_text=args.condition_on_previous_text)
-            with open(os.path.join(args.output_directory, file_name + '.srt'),
-                      "w", encoding="utf-8") as srt_file:
+                audio,
+                verbose=True,
+                language=args.language,
+                condition_on_previous_text=args.condition_on_previous_text,
+            )
+            with open(
+                os.path.join(args.output_directory, file_name + ".srt"),
+                "w",
+                encoding="utf-8",
+            ) as srt_file:
                 write_srt(result["segments"], srt_file)
 
             end_time = time.time()
             subtitle_time = end_time - start_time
             total_time += subtitle_time
             print(
-                f"Subtitles for {file} saved as {file_name}.srt in {subtitle_time:.2f}s")
-        except Exception as e:
-            print(f"Error: {e}")
+                f"Subtitles for {file} saved as {file_name}.srt in {subtitle_time:.2f}s"
+            )
+        except Exception as transcode_error:
+            print(f"Error: {transcode_error}")
             continue
 
-    print(
-        f"Generated subtitles for {len(file_array)} files in {total_time:.2f}s")
+    print(f"Generated subtitles for {len(file_array)} files in {total_time:.2f}s")
 
 
 def cli():
-    WS_DESCRIPTION = "This is a Python script that utilizes the OpenAI Whisper library to generate .srt files with subtitles for compatible files. Before running the script, ensure that the required dependencies, namely OpenAI's Whisper, are installed. The script generates .srt files for each file found in the directory using Whisper. Please note that the generated subtitles may not be completely accurate, and manual correction may be necessary."
-    parser = argparse.ArgumentParser(description=WS_DESCRIPTION)
-    parser.add_argument('--model', type=str, default='large',
-                        help='Language model to use. Options: tiny, base, small, medium, large')
-    parser.add_argument('--directory', type=str, default='.',
-                        help='Directory containing files to subtitle')
-    parser.add_argument('--output_directory', type=str,
-                        default='.', help='Directory to save subtitle files')
-    parser.add_argument('--language', type=str, default='en',
-                        help='Language to use (see Whisper documentation for options)')
-    parser.add_argument('--condition_on_previous_text', type=bool, default=False,
-                        help='Condition on previous text (see Whisper documentation)')
+    ws_description = (
+        "This is a Python script that utilizes the OpenAI Whisper library to generate"
+        " .srt files with subtitles for compatible files. Before running the script,"
+        " ensure that the required dependencies, namely OpenAI's Whisper, are"
+        " installed. The script generates .srt files for each file found in the"
+        " directory using Whisper. Please note that the generated subtitles may not be"
+        " completely accurate, and manual correction may be necessary."
+    )
+    parser = argparse.ArgumentParser(description=ws_description)
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="large",
+        help="Language model to use. Options: tiny, base, small, medium, large",
+    )
+    parser.add_argument(
+        "--directory",
+        type=str,
+        default=".",
+        help="Directory containing files to subtitle",
+    )
+    parser.add_argument(
+        "--output_directory",
+        type=str,
+        default=".",
+        help="Directory to save subtitle files",
+    )
+    parser.add_argument(
+        "--language",
+        type=str,
+        default="en",
+        help="Language to use (see Whisper documentation for options)",
+    )
+    parser.add_argument(
+        "--condition_on_previous_text",
+        type=bool,
+        default=False,
+        help="Condition on previous text (see Whisper documentation)",
+    )
     args = parser.parse_args()
     main(args)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
