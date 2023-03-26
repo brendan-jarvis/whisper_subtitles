8670ea42154845b2de7189bd209fa75a07ba4e37
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-03-26 21:08:52 +1300

    Fix args in test

--- tests/test_whisper_subtitles.py
+++ tests/test_whisper_subtitles.py
@@ -1,6 +1,7 @@
 import os
 import filecmp
 import tempfile
+import argparse
 from whisper_subtitles.main import generate_subtitles
 
 
@@ -10,36 +11,38 @@ def test_generate_subtitles():
     """
     with tempfile.TemporaryDirectory() as tmpdir:
         # Generate subtitles using the OpenAI Whisper
-        args = {
-            "model": "base.en",
-            "language": "en",
-            "input_directory": "sample/",
-            "subtitle_format": ".srt",
-            "output_directory": tmpdir,
-            "condition_on_previous_text": False,
-            "max_line_length": 42,
-            "use_cpp": False,  # Won't use whisper.cpp
-            "fp16": False,  # Avoid FP32 warning
-        }
-        generate_subtitles(args)
+        mock_args = argparse.Namespace(
+            model="base",
+            language="en",
+            input_directory="sample/",
+            subtitle_format=".srt",
+            output_directory=tmpdir,
+            condition_on_previous_text=False,
+            max_line_length=42,
+            use_cpp=False,  # Won't use whisper.cpp
+            fp16=False,  # Avoid FP32 warning
+        )
+        generate_subtitles(mock_args)
         # Compare the output SRT file to the expected SRT file
         assert filecmp.cmp(
-            "sample/test0.srt", os.path.join(tmpdir, "test0.srt"), shallow=False
+            "sample/test0.srt",
+            os.path.join(tmpdir, "test0.srt"),
+            shallow=False,
         )
     with tempfile.TemporaryDirectory() as tmpdir:
         # Generate subtitles using Whisper.cpp
-        args = {
-            "model": "base.en",
-            "language": "en",
-            "input_directory": "sample/",
-            "subtitle_format": ".srt",
-            "output_directory": tmpdir,
-            "condition_on_previous_text": False,
-            "max_line_length": 42,
-            "use_cpp": True,  # Will use whisper.cpp
-            "fp16": True,
-        }
-        generate_subtitles(args)
+        mock_args = argparse.Namespace(
+            model="base.en",
+            language="en",
+            input_directory="sample/",
+            subtitle_format=".srt",
+            output_directory=tmpdir,
+            condition_on_previous_text=False,
+            max_line_length=42,
+            use_cpp=True,  # Will use whisper.cpp
+            fp16=True,
+        )
+        generate_subtitles(mock_args)
         # Compare the output SRT file to the expected SRT file
         assert filecmp.cmp(
             "sample/test0_whisper_cpp_base.en.srt",
