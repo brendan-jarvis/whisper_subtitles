dc41b4dd956c1087b5999dd17ab5f8c1f6e987ce
Brendan Jarvis <jarvisbrendanj@gmail.com>
2023-04-04 23:19:20 +1200

    Finish split long lines function

--- whisper_subtitles/postprocessing.py
+++ whisper_subtitles/postprocessing.py
@@ -2,29 +2,24 @@
 Utility functions for the subtitle generator.
 """
 import pysubs2
-import json
 
 
 def split_long_lines(result, max_line_length=42, max_lines=2):
     """
-    Split long lines into multiple lines. Returns a SSAFile
+    Split long lines into multiple lines. Returns a SSAFile.
     """
     # Create a new SSAFile object
     subs = pysubs2.SSAFile()
 
-    # Save result to a file
-    with open("result.json", "w", encoding="utf-8") as f:
-        json.dump(result, f, indent=4)
-
     # Add the subtitles to the file as SSAEvents
     for segment in result["segments"]:
         subtitle = pysubs2.SSAEvent()
+        first_word = segment["words"][0]
+        last_word = segment["words"][-1]
+        subtitle.start = int(first_word["start"] * 1000)
+        subtitle.end = int(last_word["end"] * 1000)
 
         if len(segment["text"]) < max_line_length:
-            first_word = segment["words"][0]
-            last_word = segment["words"][-1]
-            subtitle.start = int(first_word["start"] * 1000)
-            subtitle.end = int(last_word["end"] * 1000)
             subtitle.text = segment["text"]
             subs.append(subtitle)
             continue
@@ -34,28 +29,30 @@ def split_long_lines(result, max_line_length=42, max_lines=2):
 
         for word in segment["words"]:
             if len(current_line) + len(word) + 1 > max_line_length:
-                # If the number of lines is less than the maximum number of lines
-                # Then add the current line to the lines array and reset it
                 if len(lines) < max_lines - 1:
+                    # Add the line and reset
                     lines.append(current_line)
-                    current_line = ""
+                    current_line = word["word"].strip() + " "
                 else:
+                    # Yield a SSAEvent
                     lines.append(current_line)
                     subtitle.text = "\n".join(lines)
                     last_word = segment["words"][len(lines) - 1]
                     subtitle.start = int(segment["words"][0]["start"] * 1000)
                     subtitle.end = int(last_word["end"] * 1000)
                     subs.append(subtitle)
-                    current_line = ""
+
+                    # Reset the variables
+                    current_line = word["word"].strip() + " "
+                    lines = []
                     subtitle = pysubs2.SSAEvent()
                     subtitle.start = int(word["start"] * 1000)
+                    subtitle.end = int(word["end"] * 1000)
             else:
-                current_line += word["word"]
+                current_line += word["word"].strip() + " "
                 subtitle.end = int(word["end"] * 1000)
 
-        # Add the last line
-        print(current_line)
-        print(subtitle.start, subtitle.end, subtitle.text)
+        # Add the last line in the segment
         lines.append(current_line)
         subtitle.text = "\n".join(lines)
         subs.append(subtitle)
